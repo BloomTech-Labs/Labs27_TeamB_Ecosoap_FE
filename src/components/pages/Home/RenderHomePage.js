52dc75599645118078762c4b9507a24d4e1e3a38
Genard Ivan Tejano <genardivanteajno@Genards-MacBook-Pro.local>
2020-10-12 11:49:47 -0700

    implemented navigation

--- src/components/pages/Home/RenderHomePage.js
+++ src/components/pages/Home/RenderHomePage.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { Button } from 'antd';
+import Navbar from '../../common/Navbar';
 
 function RenderHomePage(props) {
   const { userInfo, authService } = props;
   return (
     <div>
+      <Navbar></Navbar>
       <h1>Hi {userInfo.name} Welcome to Labs Basic SPA</h1>
       <div>
         <p>
